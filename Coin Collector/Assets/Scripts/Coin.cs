f7f1fe9e0b11af48cb0ea56b6ed6364631d7c95e
KingZoldack <jeanpaulismael@gmail.com>
2023-04-28 22:28:32 +0100

    Coin
    Add appropriate coin text but text shows 1 click later.
    Add coin missing 1 when bonus is given.
    TODO: Increase bonus given by 1

--- Coin Collector/Assets/Scripts/Coin.cs
+++ Coin Collector/Assets/Scripts/Coin.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI _coinText;
+
     float _currentTime;
     float _timeBeforeDestroy = 2.5f;
 
+
+    enum CoinValue
+    {
+        One = 1,
+        Ten = 10,
+        Hundred = 100
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +36,38 @@ public class Coin : MonoBehaviour
         {
             Destroy(this.gameObject);
             return;
+        }
+    }
+
+    public void HandleCoinText(int value)
+    {
+        CoinValue coinText = CoinValue.One;
+
+
+        if (value == 1)
+            coinText = CoinValue.One;
+
+        if (value == 10)
+            coinText = CoinValue.Ten;
+
+        if (value == 100)
+            coinText = CoinValue.Hundred;
+
+        Debug.Log(coinText);
+
+        switch (coinText)
+        {
+            case CoinValue.One:
+                _coinText.text = "+1";
+                break;
+            case CoinValue.Ten:
+                _coinText.text = "+10";
+                break;
+            case CoinValue.Hundred:
+                _coinText.text = "+100";
+                break;
+            default:
+                break;
         }
     }
 }
--- Coin Collector/Assets/Scripts/CoinManager.cs
+++ Coin Collector/Assets/Scripts/CoinManager.cs
@@ -16,12 +16,23 @@ public class CoinManager : MonoBehaviour
     Canvas _mainCanvas;
 
     [SerializeField]
+    Coin _coinScript;
+
+    [SerializeField]
     TextMeshProUGUI _clicksLeftText;
     [SerializeField]
     TextMeshProUGUI _coinsOwnedText;
 
+    bool _bonusGiven;
+
     int _clicksToBonus = 10;
     int _coinsEarned = 0;
+    int _oneCoinValue = 1;
+    int _tenCoinValue = 10;
+    int _hundredCoinValue = 100;
+
+    int _minBonusValue = 1, _maxBonusValue = 10;
+    int _minRangeForBonus = 1, _maxRangeForBonus = 11;
 
     // Start is called before the first frame update
     void Start()
@@ -39,19 +50,25 @@ public class CoinManager : MonoBehaviour
     {
         GameObject spawnedCoin = Instantiate(_coinPrefab, _mainCanvas.transform);
         spawnedCoin.transform.SetParent(_coinHolder.transform);
-        HandleBonus(1, 10);
+        HandleBonus(_oneCoinValue, _tenCoinValue, _hundredCoinValue);
     }
 
-    void HandleBonus(int oneCoin, int tenCoins)
+    void HandleBonus(int oneCoin, int tenCoins, int oneHundredCoins)
     {
-        _clicksToBonus--;
-        if (_clicksToBonus < 1)
-        {
-            AddCoins(tenCoins);
-            _clicksToBonus = 10;
-        }
+        _clicksToBonus--; //Tracks bonus countdown
 
-        if (_clicksToBonus >= 1)
+        if (_clicksToBonus <= 0)
+        {
+            int randomRange = Random.Range(0, 10);
+
+            if (randomRange == 0) //1 in 10 chance to get 100 bonus coins
+                AddCoins(oneHundredCoins);
+            else //9 in 10 chance to get 10 bonus coins
+                AddCoins(tenCoins);
+
+            _clicksToBonus = _maxBonusValue; //Resets Bonus Value
+        }
+        else
         {
             AddCoins(oneCoin);
         }
@@ -61,13 +78,20 @@ public class CoinManager : MonoBehaviour
 
     void AddCoins(int coinsToAdd)
     {
-        if (coinsToAdd == 1)
+        if (coinsToAdd == _oneCoinValue)
         {
             _coinsEarned++;
+            _coinScript.HandleCoinText(_oneCoinValue);
         }
-        else if (coinsToAdd == 10)
+        else if (coinsToAdd == _tenCoinValue)
         {
-            _coinsEarned += 10;
+            _coinsEarned += _tenCoinValue;
+            _coinScript.HandleCoinText(_tenCoinValue);
+        }
+        else if (coinsToAdd == _hundredCoinValue)
+        {
+            _coinsEarned += _hundredCoinValue;
+            _coinScript.HandleCoinText(_hundredCoinValue);
         }
 
         _coinsOwnedText.text = _coinsEarned.ToString();
