13bad91fdd8d9ca62d21dca2963b513423e821fb
KingZoldack <jeanpaulismael@gmail.com>
2023-04-28 08:54:18 +0100

    Coin Manager
    Implement basic bonus of 10

--- Coin Collector/Assets/Scripts/CoinManager.cs
+++ Coin Collector/Assets/Scripts/CoinManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class CoinManager : MonoBehaviour
 {
@@ -12,6 +14,14 @@ public class CoinManager : MonoBehaviour
 
     [SerializeField]
     Canvas _mainCanvas;
+
+    [SerializeField]
+    TextMeshProUGUI _clicksLeftText;
+    [SerializeField]
+    TextMeshProUGUI _coinsOwnedText;
+
+    int _clicksToBonus = 10;
+    int _coinsEarned = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +39,37 @@ public class CoinManager : MonoBehaviour
     {
         GameObject spawnedCoin = Instantiate(_coinPrefab, _mainCanvas.transform);
         spawnedCoin.transform.SetParent(_coinHolder.transform);
+        HandleBonus(1, 10);
+    }
+
+    void HandleBonus(int oneCoin, int tenCoins)
+    {
+        _clicksToBonus--;
+        if (_clicksToBonus < 1)
+        {
+            AddCoins(tenCoins);
+            _clicksToBonus = 10;
+        }
+
+        if (_clicksToBonus >= 1)
+        {
+            AddCoins(oneCoin);
+        }
+
+        _clicksLeftText.text = _clicksToBonus.ToString();
+    }
+
+    void AddCoins(int coinsToAdd)
+    {
+        if (coinsToAdd == 1)
+        {
+            _coinsEarned++;
+        }
+        else if (coinsToAdd == 10)
+        {
+            _coinsEarned += 10;
+        }
+
+        _coinsOwnedText.text = _coinsEarned.ToString();
     }
 }
